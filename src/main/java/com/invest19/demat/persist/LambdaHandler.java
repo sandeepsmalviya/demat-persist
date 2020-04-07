938cc62d1eb209749f5ba5a545bc16940e4fcd6e
Ramakrishnan <ramkicse@gmail.com>
2020-04-07 15:11:08 +0530

    sam.yaml is added

--- src/main/java/com/invest19/demat/persist/LambdaHandler.java
+++ src/main/java/com/invest19/demat/persist/LambdaHandler.java
@@ -15,7 +15,7 @@ public class LambdaHandler implements RequestStreamHandler {
 	private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
 	static {
 		try {
-			handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(DematPersistApplications.class);
+			handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(DematPersistApplication.class);
 		} catch (ContainerInitializationException e) {
 			// if we fail here. We re-throw the exception to force another cold start
 			e.printStackTrace();
